2d6822a8d409cfeaabab30c34667c4fd43cbe689
Emre Erden <emreerden@pm.me>
2024-01-25 04:14:28 +0300

    style edit main padding

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 type Props = {}
 
 const HomePageComponent = (props: Props) => {
-  return <main className="Home-Page Main-Element">HomePageComponent</main>
+  return (
+    <main className="Home-Page Main-Element Main-Padding">
+      HomePageComponent
+    </main>
+  )
 }
 
 export default HomePageComponent
