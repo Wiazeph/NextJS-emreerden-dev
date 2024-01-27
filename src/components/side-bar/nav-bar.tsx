e4e09f91cf6ccd756ef5b2653f762c0fb0171aa1
Emre Erden <emreerden@pm.me>
2024-01-27 03:42:03 +0300

    add active path class

--- src/components/side-bar/nav-bar.tsx
+++ src/components/side-bar/nav-bar.tsx
@@ -1,10 +1,16 @@
+'use client'
+
 import React from 'react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { cn } from '@/lib/utils'
 import Routes from '@/utils/consts/side-bar/routes'
 
 type Props = {}
 
 const NavBarComponent = (props: Props) => {
+  const pathname = usePathname()
+
   return (
     <div className="Nav-Bar Side-Bar-Section-Layout">
       <div className="Side-Bar-Title">Navigation</div>
@@ -14,7 +20,10 @@ const NavBarComponent = (props: Props) => {
           <Link
             key={index}
             href={route.path}
-            className="-mx-2 py-1.5 px-2 text-sm font-medium rounded-md group Main-Hover-Color"
+            className={cn(
+              '-mx-2 py-1.5 px-2 text-sm font-medium rounded-md group Main-Hover-Color',
+              pathname === route.path ? 'bg-zinc-200' : ''
+            )}
           >
             <div className="flex items-center gap-x-2 group-hover:ml-2 transition-all">
               <div className="text-xl">{route.icon}</div>
