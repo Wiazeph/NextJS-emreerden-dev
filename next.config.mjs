7cdf6b6f11fe2febe4ddcd3e67e4e4e16d208b09
Emre Erden <emreerden@pm.me>
2024-01-29 18:28:31 +0300

    add image remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.githubusercontent.com',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+        port: '',
+      },
+    ],
   },
 }
 
